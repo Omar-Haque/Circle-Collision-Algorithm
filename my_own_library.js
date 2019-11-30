3da8dcf778118cbc9b7c4172352d69dfcd0fafd8
WitchesMe <54588062+WitchesMe@users.noreply.github.com>
2019-11-30 21:38:22 +0530

    Update my_own_library.js

--- my_own_library.js
+++ my_own_library.js
@@ -1,25 +1,6 @@
 function isTouching(circle1, circle2){
- 
-    if(circle1.c + circle2.c === circle1.x + circle2.x){
-        circle1.shapeColor = "blue";
-        circle2.shapeColor = "blue";
+    if(sq(circle1.x - circle2.x) + sq(circle1.y - circle2.y) < 10000){
+        circle1.shapeColor = "red";
+        circle2.shapeColor = "red";
     }
-    
-}
-// function isTouching(circle1, circle2){
- 
-//     if(circle1.x - circle2.x < 140 
-//         && circle1.x - circle2.x > -140 
-//         && circle1.y - circle2.y < 140 
-//         && circle1.y - circle2.y > -140){
-//         circle1.shapeColor = "blue";
-//         circle2.shapeColor = "blue";
-//     }
-    
-//}
-
-function getRadius(a, b){
-    c = sqrt(a*a + b*b);
-    c = c / 2;
-    console.log(c);
 }
